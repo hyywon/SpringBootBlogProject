0b3c5cb3751bc1076d54500f90bc7407f3a7c602
hyywon <noddy0501@sch.ac.kr>
2021-07-22 16:22:17 +0900

    Spring Boot Blog Project init setting

---
+++ src/main/java/com/project/blog/domain/model/User.java
@@ -0,0 +1,31 @@
+package com.project.blog.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    int id;
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
