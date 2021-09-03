bbd8a94e7745d73a703f6d49819829c7ece90506
hyywon <noddy0501@sch.ac.kr>
2021-09-03 14:48:50 +0900

    Create Paging

--- src/main/java/com/project/blog/controller/PostController.java
+++ src/main/java/com/project/blog/controller/PostController.java
@@ -2,15 +2,24 @@ package com.project.blog.controller;
 
 import com.project.blog.config.auth.PrincipalDetail;
 import com.project.blog.domain.post.PostEntity;
+import com.project.blog.domain.post.PostRepository;
 import com.project.blog.dto.ResponseDto;
 import com.project.blog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Controller
 public class PostController {
@@ -18,9 +27,23 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping({"","/"})
-    public String index(){
+    public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        Page<PostEntity> posts = postService.게시글목록(pageable);
+        model.addAttribute("posts", posts);
         return "index";
+    }
+
+    @GetMapping("/dummy/paging")
+    public Page<PostEntity> pages(@PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<PostEntity> posts = postRepository.findAll(pageable);
+
+        List<PostEntity> postlist = posts.getContent();
+
+        return posts;
     }
 
     // USER 권한 필요
--- src/main/java/com/project/blog/service/PostService.java
+++ src/main/java/com/project/blog/service/PostService.java
@@ -4,8 +4,15 @@ import com.project.blog.domain.post.PostEntity;
 import com.project.blog.domain.post.PostRepository;
 import com.project.blog.domain.user.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Service
 public class PostService {
@@ -18,6 +25,11 @@ public class PostService {
         // title, content, user
         post.setUser(user);
         postRepository.save(post);
+    }
+
+    @Transactional
+    public Page<PostEntity> 게시글목록(Pageable pageable) {
+        return postRepository.findAll(pageable);
     }
 
 }
